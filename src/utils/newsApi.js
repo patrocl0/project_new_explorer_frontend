902ea24b2dcc5864e74c955e12c93bcfa5d5eeb6
Brayan Mora <brayanestiven1208@gmail.com>
2026-04-09 19:42:01 -0500

    Se modifica el api de news

--- src/utils/newsApi.js
+++ src/utils/newsApi.js
@@ -1,4 +1,5 @@
-const BASE_URL = "https://newsapi.org/v2";
+const BASE_URL = import.meta.env.VITE_API_URL;
+// const BASE_URL = "https://newsapi.org/v2";
 const NEWS_API_KEY = import.meta.env.NEWS_API_KEY;
 
 export const searchNews = async (search) => {
@@ -6,12 +7,11 @@ export const searchNews = async (search) => {
     const today = new Date();
     const sevenDaysAgo = new Date(today);
     sevenDaysAgo.setDate(today.getDate() - 7);
-
     const from = sevenDaysAgo.toISOString().split("T")[0];
     const to = today.toISOString().split("T")[0];
 
     const response = await fetch(
-      `${BASE_URL}/everything?q=${search}&apiKey=${"9588fa288a3d40a8a71f8343a3c0fef1"}&from=${from}&to=${to}&pageSize=100`,
+      `${BASE_URL}/news?q=${search}&from=${from}&to=${to} `,
     );
 
     if (!response.ok) {
@@ -19,6 +19,7 @@ export const searchNews = async (search) => {
     }
 
     const data = await response.json();
+
     return data.articles;
   } catch (error) {
     throw new Error("Error en búsqueda");
